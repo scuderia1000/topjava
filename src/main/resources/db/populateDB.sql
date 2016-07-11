26598c642f1600cc7395bf7c10c3af9e536e789d
scuderia1000 <a4cbepk@gmail.com>
2016-07-11 14:54:50 +0500

    HW3

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -11,3 +12,12 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (datetime, description, calories) VALUES
+  ('2015-05-30 10:00', 'Завтрак', '500'),
+  ('2015-05-30 13:00', 'Обед', '1000'),
+  ('2015-05-30 20:00', 'Ужин', '500'),
+  ('2015-05-31 10:00', 'Завтрак', '1000'),
+  ('2015-05-31 13:00', 'Обед', '500'),
+  ('2015-05-31 20:00', 'Ужин', '510')
+;
